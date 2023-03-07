e7ef884f11561b3852ade4b2bfa98a0aa3df6da5
GotinneUndam <123151588+GotinneUndam@users.noreply.github.com>
2023-03-07 15:48:36 +0100

    moving platform, ajouts de prefab du niveau tuto

--- Git_LD/Assets/Scripts/MovingPlatformScript.cs
+++ Git_LD/Assets/Scripts/MovingPlatformScript.cs
@@ -7,24 +7,34 @@ public class MovingPlatformScript : MonoBehaviour
     public Transform position_1;
     public Transform position_2;
 
-    public float speed = 1.0f;
+    public float speed = 5f;
 
     private List<GameObject> touchingObjects = new();
 
-    // Update is called once per frame
+    [SerializeField]
+    public bool isMoving;
+
+    [SerializeField]
+    private bool reverse = false;
+
     void Update()
     {
+        if (!isMoving) return;
+
         Vector3 oldPosition = transform.position;
-        transform.position = Vector3.Lerp(position_1.position, position_2.position, Mathf.PingPong(Time.time * speed, 1.0f));
+        transform.position = Vector3.MoveTowards(transform.position, (reverse ? position_1 : position_2).position, speed * Time.deltaTime);
+
+        if (transform.position == (reverse ? position_1 : position_2).position)
+            reverse = !reverse;
 
         Vector3 displacement = transform.position - oldPosition;
 
-        foreach(GameObject obj in touchingObjects)
+        foreach (GameObject obj in touchingObjects)
         {
             if (obj.transform.position.y <= transform.position.y) continue;
 
             obj.transform.position += displacement;
-            if(obj.GetComponent<CharacterController2D>())
+            if (obj.GetComponent<CharacterController2D>())
                 obj.GetComponent<CharacterController2D>().Translate(displacement);
         }
     }
@@ -37,5 +47,14 @@ public class MovingPlatformScript : MonoBehaviour
     private void OnCollisionExit2D(Collision2D collision)
     {
         touchingObjects.Remove(collision.gameObject);
+    }
+
+    public void StartMoving()
+    {
+        isMoving = true;
+    }
+    public void StopMoving()
+    {
+        isMoving = false;
     }
 }
