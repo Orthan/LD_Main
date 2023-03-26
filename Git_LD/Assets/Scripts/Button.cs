eaee2546ec2baf239f4f4864f6bfdd820fcd5eef
Gotinnne <123151588+GotinneUndam@users.noreply.github.com>
2023-03-26 21:04:00 +0200

    amélioration script bouton, Niv_0 améliorer (bientot fini)

--- Git_LD/Assets/Scripts/Button.cs
+++ Git_LD/Assets/Scripts/Button.cs
@@ -7,12 +7,20 @@ public class Button : MonoBehaviour
 {
     [SerializeField]
     public UnityEvent OnEntered;
+    public UnityEvent OnExit;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             OnEntered.Invoke();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            OnExit.Invoke();
         }
     }
 }
